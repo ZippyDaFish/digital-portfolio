55e8b29900d08ee5f8d33a73370b74c22bed4fd1
Andrew Riker <andrew.riker21@gmail.com>
2026-04-17 16:39:02 -0400

    Fix header nav links when in project view

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,15 +12,15 @@
     <div class="max-w-6xl mx-auto flex justify-between items-center p-4">
         <a href="/"><h1 class="font-bold text-lg">Andrew Riker</h1></a>
         <div class="space-x-6">
-            <a href="#projects" class="relative inline-block group transition duration-300 hover:-translate-y-1">
+            <a href="/#projects" class="relative inline-block group transition duration-300 hover:-translate-y-1">
                 <span class="text-black transition-colors duration-300">Projects</span>
                 <span class="absolute left-1/2 -bottom-1 h-0.5 w-0 bg-black transform -translate-x-1/2 transition-all duration-300 group-hover:w-full"></span>
             </a>
-            <a href="#about" class="relative inline-block group transition duration-300 hover:-translate-y-1">
+            <a href="/#about" class="relative inline-block group transition duration-300 hover:-translate-y-1">
                 <span class="text-black transition-colors duration-300">About</span>
                 <span class="absolute left-1/2 -bottom-1 h-0.5 w-0 bg-black transform -translate-x-1/2 transition-all duration-300 group-hover:w-full"></span>
             </a>
-            <a href="#contact" class="relative inline-block group transition duration-300 hover:-translate-y-1">
+            <a href="/#contact" class="relative inline-block group transition duration-300 hover:-translate-y-1">
                 <span class="text-black transition-colors duration-300">Contact</span>
                 <span class="absolute left-1/2 -bottom-1 h-0.5 w-0 bg-black transform -translate-x-1/2 transition-all duration-300 group-hover:w-full"></span>
             </a>
