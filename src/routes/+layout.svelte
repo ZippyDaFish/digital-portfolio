d862ad1eff196a0f7225bb74e8beace4d6c3f125
Andrew Riker <andrew.riker21@gmail.com>
2026-04-21 13:39:18 -0400

    Underline hover fixes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,20 +10,39 @@
 <!-- Nav -->
 <nav class="w-full border-b bg-white fixed z-10">
     <div class="max-w-6xl mx-auto flex justify-between items-center p-4">
-        <a href="/"><h1 class="font-bold text-lg">Andrew Riker</h1></a>
-        <div class="space-x-6">
-            <a href="/#projects" class="relative inline-block group transition duration-300 hover:-translate-y-1">
-                <span class="text-black transition-colors duration-300">Projects</span>
-                <span class="absolute left-1/2 -bottom-1 h-0.5 w-0 bg-black transform -translate-x-1/2 transition-all duration-300 group-hover:w-full"></span>
-            </a>
-            <a href="/#about" class="relative inline-block group transition duration-300 hover:-translate-y-1">
-                <span class="text-black transition-colors duration-300">About</span>
-                <span class="absolute left-1/2 -bottom-1 h-0.5 w-0 bg-black transform -translate-x-1/2 transition-all duration-300 group-hover:w-full"></span>
-            </a>
-            <a href="/#contact" class="relative inline-block group transition duration-300 hover:-translate-y-1">
-                <span class="text-black transition-colors duration-300">Contact</span>
-                <span class="absolute left-1/2 -bottom-1 h-0.5 w-0 bg-black transform -translate-x-1/2 transition-all duration-300 group-hover:w-full"></span>
-            </a>
+        <a href="/">
+            <h1 class="font-bold text-lg">Andrew Riker</h1>
+        </a>
+        <div class="flex gap-6">
+            <div class="group">
+                <a href="/#projects" class="relative inline-block px-3 py-2 transition-transform duration-300
+                        group-hover:-translate-y-1
+                        after:content-[''] after:absolute after:left-1/2 after:bottom-0
+                        after:h-0.5 after:w-0 after:bg-black
+                        after:-translate-x-1/2
+                        after:transition-all after:duration-300
+                        group-hover:after:w-full">Projects </a>
+            </div>
+
+            <div class="group">
+                <a href="/#about" class="relative inline-block px-3 py-2 transition-transform duration-300
+                        group-hover:-translate-y-1
+                        after:content-[''] after:absolute after:left-1/2 after:bottom-0
+                        after:h-0.5 after:w-0 after:bg-black
+                        after:-translate-x-1/2
+                        after:transition-all after:duration-300
+                        group-hover:after:w-full">About</a>
+            </div>
+
+            <div class="group">
+                <a href="/#contact" class="relative inline-block px-3 py-2 transition-transform duration-300
+                        group-hover:-translate-y-1
+                        after:content-[''] after:absolute after:left-1/2 after:bottom-0
+                        after:h-0.5 after:w-0 after:bg-black
+                        after:-translate-x-1/2
+                        after:transition-all after:duration-300
+                        group-hover:after:w-full">Contact</a>
+            </div>
         </div>
     </div>
 </nav>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -38,10 +38,16 @@
         <h2 class="text-4xl font-bold mb-4">Hello, I'm Andrew</h2>
         <p class="text-lg text-gray-600 mb-6">Full-Stack Web Developer.</p>
         <div class="space-x-4">
-            <a href="#contact" class="relative inline-block group transition duration-300 hover:-translate-y-1">
-                <span class="text-black transition-colors duration-300">Let's Get in Contact</span>
-                <span class="absolute left-1/2 -bottom-1 h-0.5 w-2/3 bg-gray-500 transform -translate-x-1/2 transition-all duration-300 group-hover:w-full group-hover:bg-black"></span>
-            </a>
+            <div class="group inline-block">
+                <a href="#contact" class="relative inline-block px-3 py-2 transition-transform duration-300
+                        group-hover:-translate-y-1
+                        after:content-[''] after:absolute after:left-1/2 after:bottom-0
+                        after:h-0.5 after:w-2/3 after:bg-gray-500
+                        after:-translate-x-1/2
+                        after:transition-all after:duration-300
+                        group-hover:after:w-full
+                        group-hover:after:bg-black">Let's Get in Contact</a>
+            </div>
         </div>
     </div>
 
